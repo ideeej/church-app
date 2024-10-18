319549c60ce0c020c1cdf219d102a6ac229cbe82
Jediel Ribeiro <39655206+ideeej@users.noreply.github.com>
2024-10-18 19:46:06 +0000

    Starting the html layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="">
-      Hello World
-    </div>
+       <div className="flex">
+        <section className="flex flex-col gap-4 h-screen">
+          <div>
+            <Link href="/" replace>Main</Link>
+          </div>
+          <div>
+            <Link href="/slides" replace>Slides</Link>
+          </div>
+          <div>
+            <Link href="/songs" replace>Músicas</Link>
+          </div>
+          <div>
+            <Link href="/videos" replace>Vídeos</Link>
+          </div>
+        </section>
+
+        <main>This is where the content is gonna go</main>
+       </div>
   );
 }
